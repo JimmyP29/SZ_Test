e5848e766c84913adb21c2bbd33243b771bfbcff
James Privett <jamesprivett29@gmail.com>
2024-01-22 12:52:59 +0000

    Soft skills being held in state, conditional rendering for loading and error message

--- frontend/src/views/SoftSkills/index.tsx
+++ frontend/src/views/SoftSkills/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import "./styles.scss";
 import { TextInputField } from "components/molecules/TextInputField";
 import { SoftSkillsList } from "./SoftSkillsList";
@@ -8,26 +8,64 @@ interface ISoftSkills { }
 
 const SoftSkills: FC<ISoftSkills> = () => {
   // TODO: 1. Implement getting soft skills from api.
+  const [softSkills, setSoftSkills] = useState<SoftSkill[]>([] as SoftSkill[]);
   const { data, loading, error } = useSoftSkillsQuery();
+  console.log(softSkills)
+  // const blah = { __typename: 'SoftSkill', id: 'CtGOY0mZZN4c6tgYj4fKB', name: 'Adapts to change', description: '' } as SoftSkill;
+  // const { data, loading, error } = useSoftSkillsQuery({
+  //   variables: {
+  //     softSkills: [blah]
+  //   },
+  // });
+
+  const retrieveSoftSkills = () => {
+    // const { data, loading, error } = useSoftSkillsQuery();
+    const result = data?.softSkills;
+
+    if (result) {
+      setSoftSkills(result as SoftSkill[]);
+    }
+  }
+
+  useEffect(() => {
+    retrieveSoftSkills();
+  });
+
 
   //console.log(data);
 
   // TODO: 3. Implement logic for searching soft skills.
 
+
+  const renderLoadingMessage = () => <p>Loading...</p>
+
+  const renderErrorMessage = () => <p>Error: {error?.message}</p>
+
   return (
     <div className="soft-skills">
       <div className="soft-skills__content">
         <h1 className="soft-skills__title">Soft Skills</h1>
-        <div className="soft-skills__actions">
-          <TextInputField
-            id="search"
-            label="Search"
-            placeholder="Search"
-            onChange={() => { }}
-            value={""}
-          />
-        </div>
-        <SoftSkillsList softSkills={data?.softSkills as SoftSkill[]} />
+        {
+          loading && renderLoadingMessage()
+        }
+        {
+          error && renderErrorMessage()
+        }
+        {
+          !loading && !error &&
+          <>
+            <div className="soft-skills__actions">
+              <TextInputField
+                id="search"
+                label="Search"
+                placeholder="Search"
+                onChange={() => { }}
+                value={""}
+              />
+            </div>
+            <SoftSkillsList softSkills={softSkills} />
+          </>
+        }
       </div>
     </div>
   );
